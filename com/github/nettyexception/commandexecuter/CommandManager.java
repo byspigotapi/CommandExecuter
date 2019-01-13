d8e382475ba9ea52b570e3dc21fd604fe057e2cd
NettyException <38795179+byspigotapi@users.noreply.github.com>
2019-01-13 11:10:06 +0100

    Add files via upload

---
+++ com/github/nettyexception/commandexecuter/CommandManager.java
@@ -0,0 +1,58 @@
+package com.github.nettyexception.commandexecuter;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class CommandManager {
+
+    /**
+     *
+     * This method starts the import with a Consumer <String> [stringConsumer] for the commands!
+     *
+     */
+
+    public static void bootThread(final Consumer<String> stringConsumer) {
+
+        final Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            stringConsumer.accept(scanner.next());
+        }
+
+    }
+
+    /**
+     *
+     * In this method, all commands are saved with a string, for example: '!shutdown' or '!help'.
+     *
+     */
+
+    public static void registerCommands() {
+
+        bootThread( string -> {
+
+            if (string.equalsIgnoreCase("!shutdown")) {
+                // @TODO Implements ShutdownMethod!
+            }
+
+            if (string.equalsIgnoreCase("!help")) {
+
+                System.out.println("Here you can set your own text message!");
+
+            }
+
+        });
+
+    }
+
+    /**
+     *
+     * In this method, start up the command manager!
+     *
+     */
+
+    public static void main(String[] args) {
+        CommandManager.registerCommands();
+    }
+
+}
